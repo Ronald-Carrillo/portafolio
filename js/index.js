de8132893600d7fd04b3e70f125e403e3aae0201
Ronald Carrillo <rcarrilloparra2016@gmail.com>
2021-07-21 20:47:42 -0500

    update two

--- js/index.js
+++ js/index.js
@@ -44,3 +44,26 @@ window.addEventListener("scroll", () => {
     navBar.classList.remove("fix-nav");
   }
 });
+
+// contact
+
+VanillaTilt.init(document.querySelectorAll(".sci li a"), {
+  max: 30,
+  speed: 400,
+  glare: true,
+  "max-glare": 1,
+});
+let list = document.querySelectorAll(".sci li");
+let bg = document.querySelector(".social-media ");
+let title = document.querySelector("title-contact");
+list.forEach((elements) => {
+  elements.addEventListener("mouseenter", function (event) {
+    let color = event.target.getAttribute("data-color");
+    bg.style.backgroundColor = color;
+    title.style.backgroundColor = color;
+  });
+  elements.addEventListener("mouseleave", function (event) {
+    let color = event.target.getAttribute("data-color");
+    bg.style.backgroundColor = "#fff";
+  });
+});
